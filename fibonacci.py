fdf6cda6310d8509a1dee83ea832395c536af220
Mr Fong <75341864+fong-a@users.noreply.github.com>
2022-02-27 19:20:09 +1100

    Fibonacci Template

---
+++ fibonacci.py
@@ -0,0 +1,14 @@
+'''
+The Fibonacci Sequence is the series of numbers:
+0,1,1,2,3,5,8,13,21,34,...0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...0,1,1,2,3,5,8,13,21,34,...
+The series starts with 000 and 111 as the first two numbers in the series. The next number in the series is found by summing the two numbers before it.
+
+Create a function that will return x number of numbers in the series
+
+'''
+
+def fib(x):
+    pass
+
+
+fib(10)
